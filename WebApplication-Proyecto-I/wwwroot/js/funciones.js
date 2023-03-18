0bbc6ee3e1adf7d26a2fa6e5fb79f0cbd7c8a131
IsaacDavidMoreraVargas <isaacmorerawrath@hotmail.com>
2023-03-18 00:52:42 -0600

    anadida paso 1 al 4

--- WebApplication-Proyecto-I/wwwroot/js/funciones.js
+++ WebApplication-Proyecto-I/wwwroot/js/funciones.js
@@ -3,6 +3,7 @@
 function check_Empty() {
     let array_inputs = document.getElementsByTagName("input");
     color_border(array_inputs, "1px solid #F5524C", "1px solid black")
+    columnas_esconder()
 }
 function color_border(array, colorSi, colorNo) {
 
@@ -46,4 +47,38 @@ function show_ul_options(id_element)
         document.getElementById(id_element).getElementsByTagName("ul")[0].style.display = "block";
     }
 
+}
+let flag_columna = 0;
+let array_columnas = 0;
+function columnas_esconder()
+{
+    array_columnas = document.getElementsByClassName("contenedor-columna");
+    for (let flag = 0; flag < array_columnas.length; flag++) {
+        array_columnas[flag].style.display = "none";
+    }
+    array_columnas[flag_columna].style.display = "block";
+    let inicio = flag_columna + 1;
+    let final = array_columnas.length; //+ 1;
+    document.getElementsByClassName("leyenda-flechas")[0].textContent = "Paso " + inicio + " de " + final;
+}
+
+function a_la_izquierda()
+{
+    --flag_columna;
+    if (flag_columna < 0)
+    {
+        flag_columna = array_columnas.length-1;
+    }
+    columnas_esconder()
+    //alert("here" + flag_columna);
+}
+
+function a_la_derecha()
+{
+    ++flag_columna;
+    if (flag_columna ==array_columnas.length) {
+        flag_columna = 0;
+    }
+    columnas_esconder()
+    //alert("here2" + flag_columna);
 }
