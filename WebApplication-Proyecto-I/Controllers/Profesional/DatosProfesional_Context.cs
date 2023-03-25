4d19c60958cb19f45a62103cd5e4f20ed9a1432f
IsaacDavidMoreraVargas <isaacmorerawrath@hotmail.com>
2023-03-25 00:20:06 -0600

    anadido calendario, esqueleto conexion base de datos, paises y provincias, arreglo comportamiento botnoes guardar

---
+++ WebApplication-Proyecto-I/Controllers/Profesional/DatosProfesional_Context.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication_Proyecto_I.Controllers.Profesional
+{
+    public partial class DatosProfesional_Context : DbContext
+    {
+        public DatosProfesional_Context() { }
+        public DatosProfesional_Context(DbContextOptions<DatosProfesional_Context> options) : base(options) { }
+        public virtual DbSet<WebApplication_Proyecto_I.Models.Profesional.asociar_profesional> Registros_Profesional{ get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-RVLLMUB;Database=TareaII;Trusted_Connection=True;Encrypt=false;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WebApplication_Proyecto_I.Models.Profesional.asociar_profesional>(entity =>
+            {
+                entity.ToTable("");
+                entity.Property(e => e.fechaRegistro)
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebApplication-Proyecto-I/Models/Profesional/asociar_profesional.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication_Proyecto_I.Models.Profesional
+{
+    public partial class asociar_profesional
+    {
+        [Key]
+        public int idRegistro { get; set; }
+
+        public int idEmpleadoAsociado { get; set; }
+
+        public int idPuestoAsociado { get; set; }
+
+        public string? fechaRegistro { get; set; }
+    }
+}
